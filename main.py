dc286dc3ef55e408e3f1f93a0357162744847633
Igor Maurício <igor.mauricio.eng@gmail.com>
2025-05-26 17:52:46 -0300

    Adicionada mensagem de boas vindas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+print("Bem-vindo a minha calculadora")
 print("Digite o primeiro numero")
 primeiro_numero = float(input())
 print("Digite o segundo numero")
 segundo_numero = float(input())
 
-print("Soma = ", primeiro_numero + segundo_numero)
+print("Soma = ", primeiro_numero - segundo_numero)
