b2028dbb386476b4255c55dae3f7cd6b040c835f
Igor Maurício <igor.mauricio.eng@gmail.com>
2025-05-26 17:58:56 -0300

    revertido commit de soma

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-print("Bem-vindo a minha calculadora")
-print("Digite o primeiro numero")
-primeiro_numero = float(input())
-print("Digite o segundo numero")
-segundo_numero = float(input())
 
-print("Soma = ", primeiro_numero - segundo_numero)
+print("hello world")
