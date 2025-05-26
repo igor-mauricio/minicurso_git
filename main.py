a6fb8e699dbfc0e138798b2bfa547983ec936c16
Igor Maurício <igor.mauricio.eng@gmail.com>
2025-05-26 18:02:13 -0300

    Adicionada mensagem de ok no final

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ print("Bem-vindo a minha calculadora")
 print("Digite o primeiro numero")
 primeiro_numero = float(input())
 print("Digite o segundo numero")
+
 segundo_numero = float(input())
 
 print("Soma = ", primeiro_numero + segundo_numero)
+
+print("ok")
